732e7295566286a2c78f456779bbd0405f6a28cd
Filip Troníček <filip.tronicek@seznam.cz>
2020-06-09 22:54:24 +0200

    Create replace.py

---
+++ replace.py
@@ -0,0 +1,9 @@
+import json
+with open("README.md") as f:
+    lines = f.read()
+with open("config.json") as a:
+    jsson = a.read()
+    jsson = json.loads(jsson)
+
+
+print(lines.replace(jsson["templating"], ))
